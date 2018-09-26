99201ea9717be3b4ae68d9aea1325e85e55b6ec5
padrisimo <adrianfernandezchamizo@gmail.com>
2018-09-27 01:19:13 +0200

    counter component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import { TimesCounter } from './Components/TimesCounter';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <TimesCounter total={50}
+        powder={20}
+        backcountry={10}
+        goal={100} />
     );
   }
 }
