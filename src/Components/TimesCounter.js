3d21c2225330df0f95309fb1b96e0e4b2440aa8d
padrisimo <adrianfernandezchamizo@gmail.com>
2018-10-02 14:51:18 +0200

    prop-types

--- src/Components/TimesCounter.js
+++ src/Components/TimesCounter.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import { TiWaves, TiWeatherPartlySunny } from 'react-icons/ti';
+import { TiWaves, TiAdjustBrightness } from 'react-icons/ti';
 import { GoCalendar } from 'react-icons/go';
-import '../Styles/ui.scss'
+import PropTypes from 'prop-types';
+import '../Styles/ui.scss';
 
-const percentToDecimal = decimal => ((decimal * 100) + '%');
+const percentToDecimal = decimal => ((decimal * 100).toFixed(1) + '%');
 
 const calcGoalProgress = (total, goal) => percentToDecimal(total / goal);
 
 
-export const TimesCounter = ({ surf, goal, sunny, total }) => (
+export const TimesCounter = ({ surf=25, goal=75, sunny=24, total=50 }) => (
     <div className="ski-day-count">
         <div className="total-days">
             <span>{total} </span>
-            <GoCalendar />
+            <GoCalendar style={{color: 'SlateGray'}}/>
             <span>days</span>
         </div>
         <div className="powder-days">
             <span>{sunny}</span>
-            <TiWeatherPartlySunny />
+            <TiAdjustBrightness style={{color: 'gold'}}/>
             <span>days</span>
         </div>
         <div className="backcountry-days">
             <span>{surf}</span>
-            <TiWaves />
+            <TiWaves style={{color: 'DodgerBlue'}}/>
             <span>days</span>
         </div>
         <div>
@@ -36,9 +37,18 @@ export const TimesCounter = ({ surf, goal, sunny, total }) => (
     </div>
 );
 
+TimesCounter.propTypes = {
+    total: PropTypes.number,
+    sunny: PropTypes.number,
+    surf: PropTypes.number,
+}
+
+
+/* 
 TimesCounter.defaultProps = {
     total: 20,
     sunny: 10,
     surf: 25,
     goal: 50
-}
+} 
+*/
